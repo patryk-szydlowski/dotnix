7b4cbefaf238a9562784dddcbd9d17f28d57c689
Patryk Szydłowski <no-reply@patrykszydlowski.com>
2024-01-11 16:33:11 +0000

    refactor(libraries): remove additional spacing

--- libraries/modules/default.nix
+++ libraries/modules/default.nix
@@ -1,8 +1,6 @@
 { inputs }:
 let
   config = import ./config.nix { inherit inputs path; };
-
   option = import ./option.nix { inherit inputs path; };
-
   path = import ./path.nix { inherit inputs; };
 in { inherit config option; }
